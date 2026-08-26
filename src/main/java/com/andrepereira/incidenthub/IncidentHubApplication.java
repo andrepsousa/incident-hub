9c548385df2f4db4ec2a0f2a1f2acea7bf9fc4a5
Andre <andre.pds07@gmail.com>
2026-08-26 15:18:20 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/andrepereira/incidenthub/IncidentHubApplication.java
@@ -0,0 +1,13 @@
+package com.andrepereira.incidenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrepereira/incidenthub/IncidentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrepereira.incidenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
