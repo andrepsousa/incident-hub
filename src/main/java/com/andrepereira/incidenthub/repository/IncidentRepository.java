5ee77a488076cd2a18ca6bd5b32573cddb1efa65
Andre <andre.pds07@gmail.com>
2026-08-31 15:13:02 -0300

    feat: add incident code sequence

--- src/main/java/com/andrepereira/incidenthub/repository/IncidentRepository.java
+++ src/main/java/com/andrepereira/incidenthub/repository/IncidentRepository.java
@@ -2,6 +2,7 @@ package com.andrepereira.incidenthub.repository;
 
 import com.andrepereira.incidenthub.domain.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +12,7 @@ public interface IncidentRepository extends JpaRepository<Incident, UUID> {
     Optional<Incident> findByCode(String code);
 
     boolean existsByCode(String code);
+
+    @Query(value = "SELECT nextval('incident_code_seq')", nativeQuery = true)
+    Long nextCodeSequence();
 }
---
+++ src/main/resources/db/migration/V2__create_incident_code_sequence.sql
@@ -0,0 +1,3 @@
+CREATE SEQUENCE incident_code_seq
+    START WITH 1
+    INCREMENT BY 1;
